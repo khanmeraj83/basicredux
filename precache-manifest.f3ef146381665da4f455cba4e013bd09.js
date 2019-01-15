10793754a9b3cad0cbc69a434ebd15ce81c64788
Meraj khan <khanmeraj83@gmail.com>
2019-01-15 18:10:50 +0500

    Updates

---
+++ precache-manifest.f3ef146381665da4f455cba4e013bd09.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eeb5517a7a750e587e6",
+    "url": "/basicredux/static/js/runtime~main.2eeb5517.js"
+  },
+  {
+    "revision": "86cd3676669babddb26b",
+    "url": "/basicredux/static/js/main.86cd3676.chunk.js"
+  },
+  {
+    "revision": "eb5a6a5a6336a6e05669",
+    "url": "/basicredux/static/js/1.eb5a6a5a.chunk.js"
+  },
+  {
+    "revision": "a9008fd96ffbaf575b7609d18a572a4a",
+    "url": "/basicredux/index.html"
+  }
+];
